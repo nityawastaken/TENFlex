bbaee3f4fb91937819302c96f328603db107ec44
Krishan Kant <145056066+krishan086@users.noreply.github.com>
2025-06-07 17:51:21 +0530

    Delete reviewsportal directory

--- reviewsportal/reviews/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Review, Gig
-
-@admin.register(Review)
-class ReviewAdmin(admin.ModelAdmin):
-    list_display = ['id', 'user', 'gig', 'rating', 'created_at']
-
-admin.site.register(Gig)
