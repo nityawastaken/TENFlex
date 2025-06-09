cb784264667f55934bda467b60b5f1e3155cce62
Sneha Mazumder <snehamaz7@gmail.com>
2025-06-09 16:43:41 +0300

    Update views.py

--- base/views.py
+++ base/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import SignUpForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form}) # Name of the HTML file can be changed according to the frontend.
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form}) # Name of the HTML file can be changed according to the frontend.
