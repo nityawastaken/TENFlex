53612b22c1e0933e56df939f3242ba6af9ac65e6
Mehnaz Ali <mehnazali2004@gmail.com>
2025-08-05 20:48:10 +0530

    bug fix: Images loading

--- TENFlex_backend/urls.py
+++ TENFlex_backend/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 from rest_framework.authtoken.views import obtain_auth_token
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,3 +29,8 @@ urlpatterns = [
     # path('api/', include('reviews.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
