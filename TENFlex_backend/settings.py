3dd16e8b2c0b3ac09f80fe21deb62c054bc73eb7
Mehnaz Ali <mehnazali2004@gmail.com>
2025-08-05 20:47:22 +0530

    bug fix: Images loading

--- TENFlex_backend/settings.py
+++ TENFlex_backend/settings.py
@@ -169,3 +169,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'base.CustomUser'
+
+# Media files (User uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
