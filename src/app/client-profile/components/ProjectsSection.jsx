7a5d5ec5af88261600148cc5e15c2201a680dfb4
Ninad-arakh <arakhninad@gmail.com>
2025-08-09 21:24:31 +0530

    reviews UI fix

--- src/app/client-profile/components/ProjectsSection.jsx
+++ src/app/client-profile/components/ProjectsSection.jsx
@@ -21,7 +21,7 @@ const ProjectsSection = ({ refProp, projects, setProjects, id }) => {
       }
     );
     setProjects(response.data);
-    console.log("projects : ", response.data)
+    // console.log("projects : ", response.data)
   };
 
 
--- src/app/client-profile/components/ReviewCard.jsx
+++ src/app/client-profile/components/ReviewCard.jsx
@@ -38,7 +38,7 @@ const ReviewCard = ({ r }) => {
   return (
     <div
       key={r.id}
-      className="text-white bg-gradient-to-br from-[#24194a] via-[#1a0d2b] to-[#28163a] px-6 py-4 rounded-2xl shadow-lg flex flex-col md:flex-row gap-4 items-start border border-purple-900 min-h-[120px] md:h-[20vh] transition-transform hover:scale-[1.02] hover:shadow-2xl"
+      className="text-white bg-gradient-to-br from-[#24194a] via-[#1a0d2b] to-[#28163a] px-6 py-4 rounded-2xl shadow-lg flex flex-col md:flex-row gap-4 items-start border border-purple-900 min-h-[120px] md:h-[20vh] transition-transform hover:scale-[1.02] hover:shadow-2xl overflow-y-scroll scrollbar-hide "
     >
       <div className="flex-shrink-0 flex flex-col items-center justify-center w-20 ">
         <div className="bg-purple-900 rounded-full w-14 h-14 flex items-center justify-center mb-2 ">
@@ -48,20 +48,20 @@ const ReviewCard = ({ r }) => {
       </div>
       <div className="flex-1">
         <div className="flex flex-col md:flex-row md:justify-between gap-2">
-          <p>
+          <div>
             <span className="text-sm ">Freelancer:</span>{" "}
             <h2
               onClick={() => handleOpenProfile(gigDetails?.freelancer)}
-              className="cursor-pointer text-purple-200 text-sm hover:underline underline hover:-translate-y-0.5 hover:text-purple-500 duration-300"
+              className="cursor-pointer text-purple-200 text-sm hover:underline underline hover:-translate-y-0.5 hover:text-purple-500 duration-300 truncate"
             >
               {gigDetails?.freelancer}
             </h2>
-          </p>
+          </div>
           <p>
             <span className="text-sm ">Gig : </span>
             <Link
               href={`/gigDetails/${r.gig_id}`}
-              className="text-sm cursor-pointer text-purple-200 hover:underline"
+              className="text-sm cursor-pointer text-purple-200 hover:underline truncate"
             >
               {r.gig_title}
             </Link>
@@ -71,7 +71,7 @@ const ReviewCard = ({ r }) => {
           <span className="font-medium">Posted:</span>{" "}
           {new Date(r.created_at).toLocaleDateString()}
         </p>
-        <div className="bg-purple-950/40 rounded-xl p-3 text-gray-100 text-base overflow-y-auto min-h-[60px] md:h-[8vh] mt-2">
+        <div className="bg-purple-950/40 flex flex-wrap rounded-xl p-3 text-gray-100 text-base  break-words  mt-2 max-w-full md:max-w-[40vw] md:min-h-[60px] h-24 overflow-y-auto md:h-auto">
           {r.comment}
         </div>
       </div>
